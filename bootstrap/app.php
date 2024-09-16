afe541ef0126be2996f491dd689f7533e74aed7f
SetUp <abdulmumeenibroheem51@gmail.com>
2024-09-16 13:14:37 +0100

    Feat:configured-admin-middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\premiumcoursesmiddleware;
+use App\Http\Middleware\Adminmiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,7 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->alias([
             // my-other-middleware
-            'premiumuser' => premiumcoursesmiddleware::class
+            'premiumuser' => premiumcoursesmiddleware::class,
+            'admin' => Adminmiddleware::class
         ]);
 
         $middleware->append(RoleMiddleware::class);
