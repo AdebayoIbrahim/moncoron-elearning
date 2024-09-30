8c5403f056d28f3747dcc186243d5b5ef4ab56d4
SetUp <abdulmumeenibroheem51@gmail.com>
2024-09-30 11:13:04 +0100

    Feat:prev-button-goes-tothelast-question-on

--- resources/js/assessment-take/index.js
+++ resources/js/assessment-take/index.js
@@ -1,46 +1,40 @@
-// Initialize with the first question displayed
 const questions = document.querySelectorAll(".area-question-data");
 questions[0].style.display = "block";
 
-// Get the current question index (assuming it's starting at 1)
 let currentQuestionIndex = parseInt(
     document.querySelector(".question_current_index")?.id
 );
 
-// Show the correct question based on the index
 function showQuestion(index) {
     questions.forEach((question, i) => {
         question.style.display = i === index ? "block" : "none";
     });
 }
 
-// Update the displayed question on navigation
 const btnprogress = document
     .querySelectorAll("#box_navigate_cbt")
     .forEach((btn) => {
         btn.addEventListener("click", (e) => {
-            const currid = parseInt(btn.innerText); // Get the question index from the button text
-            currentQuestionIndex = currid; // Update current index (convert to zero-based)
-            showQuestion(currentQuestionIndex); // Show the selected question
+            const currid = parseInt(btn.innerText);
+            currentQuestionIndex = currid;
+            showQuestion(currentQuestionIndex);
         });
     });
 
-// Handle next and prev buttons
 const next = document.querySelector("#next_cbt");
 const prev = document.querySelector("#prev_cbt");
 
 next.onclick = function () {
     if (currentQuestionIndex < questions.length - 1) {
-        currentQuestionIndex++; // Move to the next question
-        showQuestion(currentQuestionIndex); // Display the next question
+        currentQuestionIndex++;
+        showQuestion(currentQuestionIndex);
     }
 };
 
 prev.onclick = function () {
-    if (currentQuestionIndex > 0) {
-        currentQuestionIndex--; // Move to the previous question
-        showQuestion(currentQuestionIndex); // Display the previous question
-    }
+    currentQuestionIndex--;
+    currentQuestionIndex < 0 && (currentQuestionIndex = questions.length - 1);
+    showQuestion(currentQuestionIndex);
 };
 
 // Show the first question initially
