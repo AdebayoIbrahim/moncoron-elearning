055a0bab5ebdc1e3dfc59549386274e9fb686de9
Ibrahim <98415538+AdebayoIbrahim@users.noreply.github.com>
2024-10-19 08:47:44 +0100

    Update database.php
    disable ssl checks

--- config/database.php
+++ config/database.php
@@ -54,9 +54,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+             'options' => [
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,  // Disable SSL verification
+            ],
+            // 'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            // ]) : [],
         ],
 
         'mariadb' => [
